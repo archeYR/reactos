5f4791c2cfbaa8a44b805411ded2aabfe3a5bfad
Hermès Bélusca-Maïto <hermes.belusca-maito@reactos.org>
2020-08-20 13:58:16 +0200

    Revert "[REACTOS] Raise C standard to gnu99"
    This reverts commit 95483b42ea1b45dc11620ce139b8456ee9cdda76.

--- sdk/lib/pseh/include/pseh/pseh3.h
+++ sdk/lib/pseh/include/pseh/pseh3.h
@@ -16,6 +16,12 @@
 extern "C" {
 #endif
 
+#ifdef __cplusplus
+#define PRAGMA_DIAGNOSTIC_IGNORED_DECLARATION_AFTER_STATEMENT
+#else
+#define PRAGMA_DIAGNOSTIC_IGNORED_DECLARATION_AFTER_STATEMENT _Pragma("GCC diagnostic ignored \"-Wdeclaration-after-statement\"")
+#endif
+
 /* CLANG must safe non-volatiles, because it uses a return-twice algorithm */
 #if defined(__clang__) && !defined(_SEH3$_FRAME_ALL_NONVOLATILES)
 #define _SEH3$_FRAME_ALL_NONVOLATILES 1
@@ -358,6 +364,9 @@ _Pragma("GCC diagnostic pop") \
         (void)&&_SEH3$_l_BeforeFilterOrFinally; \
         (void)&&_SEH3$_l_FilterOrFinally; \
 \
+_Pragma("GCC diagnostic push") \
+PRAGMA_DIAGNOSTIC_IGNORED_DECLARATION_AFTER_STATEMENT \
+\
         /* Count the try level. Outside of any __try, _SEH3$_TryLevel is 0 */ \
         enum { \
             _SEH3$_PreviousTryLevel = _SEH3$_TryLevel, \
@@ -369,6 +378,8 @@ _Pragma("GCC diagnostic pop") \
 \
         /* Allocate a registration frame */ \
         volatile SEH3$_REGISTRATION_FRAME _SEH3$_AUTO_CLEANUP _SEH3$_TrylevelFrame; \
+\
+_Pragma("GCC diagnostic pop") \
 \
         goto _SEH3$_l_BeforeTry; \
         /* Silence warning */ goto _SEH3$_l_AfterTry; \
@@ -384,6 +395,9 @@ _Pragma("GCC diagnostic pop") \
 \
     _SEH3$_l_BeforeTry: (void)0; \
         _SEH3$_ASM_GOTO(_SEH3$_l_OnException); \
+\
+_Pragma("GCC diagnostic push") \
+PRAGMA_DIAGNOSTIC_IGNORED_DECLARATION_AFTER_STATEMENT \
 \
         /* Forward declaration of the filter function */ \
         _SEH3$_DECLARE_FILTER_FUNC(_SEH3$_FilterFunction); \
@@ -432,6 +446,9 @@ _Pragma("GCC diagnostic pop") \
     _SEH3$_l_BeforeTry: (void)0; \
         _SEH3$_ASM_GOTO(_SEH3$_l_OnException); \
 \
+_Pragma("GCC diagnostic push") \
+PRAGMA_DIAGNOSTIC_IGNORED_DECLARATION_AFTER_STATEMENT \
+\
         /* Forward declaration of the finally function */ \
         _SEH3$_DECLARE_FILTER_FUNC(_SEH3$_FinallyFunction); \
 \
@@ -467,6 +484,8 @@ _Pragma("GCC diagnostic pop") \
 \
         /* Implementation of the auto cleanup function */ \
         _SEH3$_DEFINE_CLEANUP_FUNC(_SEH3$_AutoCleanup); \
+\
+_Pragma("GCC diagnostic pop") \
 \
     /* Close the outer scope */ \
     }
