2212851c296c1c4f2b17f31e4daea0216b6bee5b
Adam Słaboń <asaillen@protonmail.com>
2022-04-19 03:54:26 +0200

    [DESKPERF]

---
+++ dll/shellext/deskperf/resource.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#define IDC_MONITORICO   100
+
+#define IDD_TROUBLESHOOT  200
+#define IDD_COMPATIBILITY 250
+
+#define IDC_ACCELERATION_LEVEL       201
+#define IDC_ACCELERATION_LEVEL_TEXT  202
+#define IDC_WRITE_COMBINING          205
+#define IDC_REBOOT_REQUIRED          251
+#define IDC_WITHOUT_REBOOT           252
+#define IDC_SAVE_CDSPREF             253
+
+
+#define IDS_LEVEL0          300
+#define IDS_LEVEL1          301
+#define IDS_LEVEL2          302
+#define IDS_LEVEL3          303
+#define IDS_LEVEL4          304
+#define IDS_LEVEL5          305
+#define IDS_USWC_TITLE      310
+#define IDS_USWC_DESC       311
+#define IDS_REG_ERROR_TITLE 312
+#define IDS_REG_ERROR_DESC  313
