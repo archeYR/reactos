996896bf5b6106582e5dfae3e8068e13816d9738
Adam Słaboń <asaillen@protonmail.com>
2024-03-02 23:56:51 +0100

    [FORMAT] Use QueryDeviceInformation for retrieving the volume size before formatting it
    QueryDeviceInformation returns more reliable volume length and works
    with unformatted volumes. It will return volume length only on ROS and Vista+ however,
    so also keep the GetFreeDiskSpaceExW as a fallback for XP/2003.

--- base/system/format/format.c
+++ base/system/format/format.c
@@ -360,6 +360,7 @@ static VOID Usage(LPWSTR ProgramName)
 int wmain(int argc, WCHAR *argv[])
 {
     int badArg;
+    DEVICE_INFORMATION DeviceInformation;
     FMIFS_MEDIA_FLAG media = FMIFS_HARDDISK;
     DWORD driveType;
     WCHAR fileSystem[1024];
@@ -479,7 +480,22 @@ int wmain(int argc, WCHAR *argv[])
         return -1;
     }
 
-    if (!GetDiskFreeSpaceExW(RootDirectory,
+    /* Get the volume size */
+    totalNumberOfBytes.QuadPart = 0;
+
+    if (QueryDeviceInformation(RootDirectory,
+                               &DeviceInformation,
+                               sizeof(DeviceInformation)))
+    {
+        totalNumberOfBytes.QuadPart = DeviceInformation.SectorSize *
+                                    DeviceInformation.SectorCount.QuadPart;
+    }
+
+    /* QueryDeviceInformation returns more accurate volume length and works with
+     * unformatted volumes, however it will NOT return volume length on XP/2003.
+     * Fallback to GetFreeDiskSpaceExW if we did not get any volume length. */
+    if (totalNumberOfBytes.QuadPart == 0 &&
+        !GetDiskFreeSpaceExW(RootDirectory,
                              &freeBytesAvailableToCaller,
                              &totalNumberOfBytes,
                              &totalNumberOfFreeBytes))
