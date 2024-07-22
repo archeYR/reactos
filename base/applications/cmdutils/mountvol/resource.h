15be4cfb2b67a0e6197b9dcf0afaabc242db78dd
Adam Słaboń <asaillen@protonmail.com>
2024-07-22 14:38:40 +0200

    [MOUNTVOL][WIP] Implement mountvol utility

---
+++ base/applications/cmdutils/mountvol/resource.h
@@ -0,0 +1,9 @@
+#include <windef.h>
+#pragma once
+
+/* Translation IDs */
+
+/* Shared */
+#define STRING_MOUNTVOL_USAGE                    100
+#define STRING_MOUNTVOL_LIST                     101
+#define STRING_MOUNTVOL_NOPOINTS                 102
