cb3f9ab0166c79a073b949bc150e87f7759606b8
Adam Słaboń <asaillen@protonmail.com>
2024-09-02 21:58:41 +0200

    [MOUNTVOL] Full implementation
    Feature parity with Windows Vista equivalent.

--- base/applications/cmdutils/mountvol/resource.h
+++ base/applications/cmdutils/mountvol/resource.h
@@ -5,5 +5,14 @@
 
 /* Shared */
 #define STRING_MOUNTVOL_USAGE                    100
-#define STRING_MOUNTVOL_LIST                     101
-#define STRING_MOUNTVOL_NOPOINTS                 102
+#define STRING_MOUNTVOL_HELP                     101
+#define STRING_MOUNTVOL_ESPMOUNTUSAGE            102
+#define STRING_MOUNTVOL_ESPMOUNTHELP             103
+#define STRING_MOUNTVOL_LIST                     104
+#define STRING_MOUNTVOL_NOPOINTS                 105
+#define STRING_MOUNTVOL_NOTMOUNTABLE             106
+#define STRING_MOUNTVOL_NOAUTOMOUNT              107
+#define STRING_MOUNTVOL_TOOMANYPOINTS            108
+#define STRING_MOUNTVOL_UNSUPPORTEDOPERATION     109
+#define STRING_MOUNTVOL_VOLUMESTILLINUSE         110
+#define STRING_MOUNTVOL_ESPMOUNTPOINT            111
